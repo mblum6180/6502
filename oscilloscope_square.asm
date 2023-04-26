297c2b0bf56833a1b5f1b1901f1472c3120dd594
Matthew Blum <45472455+mblum6180@users.noreply.github.com>
2023-04-26 15:57:08 -0400

    start

--- oscilloscope_square.asm
+++ oscilloscope_square.asm
@@ -1,1 +1,173 @@
+; Define memory addresses for the 6522 VIA registers
+VIA_PORTA   = $6000
+VIA_PORTB   = $6001
 
+; Define initial coordinates and size of the square
+Size        = $20
+CenterX     = $40
+CenterY     = $40
+
+; Define rotation speed and movement speed
+RotationSpeed = 1
+MoveSpeedX    = 3
+MoveSpeedY    = 2
+
+; Main program
+        * = $8000
+start:
+    LDX #0
+    STX VIA_PORTA
+    STX VIA_PORTB
+
+; Initialize variables for angle, position, and size
+Angle      = $00
+PosX       = CenterX
+PosY       = CenterY
+
+Loop:
+    ; Update angle
+    CLC
+    LDA Angle
+    ADC #RotationSpeed
+    STA Angle
+
+    ; Update position
+    CLC
+    LDA PosX
+    ADC #MoveSpeedX
+    STA PosX
+
+    CLC
+    LDA PosY
+    ADC #MoveSpeedY
+    STA PosY
+
+    ; Draw the rotating and moving square
+    LDX #4
+DrawLoop:
+    JSR DrawVertex
+    DEX
+    BNE DrawLoop
+
+    JMP Loop
+
+; Draw a vertex of the square at the current angle and position
+DrawVertex:
+    ; Calculate sine and cosine for the angle
+    LDA Angle
+    JSR Cordic
+
+    ; Multiply the results by the size
+    LDX Size
+    JSR Multiply
+
+    ; Add the offsets to the position
+    CLC
+    LDA PosX
+    ADC ResultX
+    STA VIA_PORTA
+    LDA PosY
+    ADC ResultY
+    STA VIA_PORTB
+
+    ; Delay
+    JSR Delay
+    RTS
+
+; Delay subroutine to pause between coordinate updates
+Delay:
+    LDY #0
+DelayLoop:
+    LDX #0
+DelayInnerLoop:
+    DEX
+    BNE DelayInnerLoop
+    DEY
+    BNE DelayLoop
+    RTS
+
+; CORDIC algorithm for fixed-point sine and cosine
+; Input: A = angle (0-255, scaled to 0-360 degrees)
+; Output: ResultX = cosine, ResultY = sine (8-bit fixed-point numbers)
+Cordic:
+    STA CurrentAngle
+    LDX #128
+    STX ResultX
+    LDY #0
+    STY ResultY
+
+    ; Perform CORDIC iterations
+    LDY #1
+    LDA #0
+CordicLoop:
+    ; Check if we need to rotate left or right
+    CMP CurrentAngle
+    BCS RotateRight
+
+RotateLeft:
+    ; Rotate left
+    SEC
+    SBC CurrentAngle
+    STA CurrentAngle
+    LSR
+    TAX
+    LDA ResultY
+    SEC
+    SBC ResultX
+    STA Temp
+    LDA ResultX
+    ADC ResultY
+    STA ResultY
+    LDA Temp
+    STA ResultX
+    INY
+    JMP CordicContinue
+
+RotateRight:
+    ; Rotate right
+    SBC CurrentAngle
+    STA CurrentAngle
+    LSR
+    TAX
+    LDA ResultY
+    SEC
+    SBC ResultX
+    STA Temp
+    LDA ResultX
+    ADC ResultY
+    STA ResultY
+    LDA Temp
+    STA ResultX
+    INY
+
+CordicContinue:
+    ; Check if more iterations are needed
+    CPY #8
+    BNE CordicLoop
+
+    RTS
+
+; Multiply subroutine: multiply 8-bit numbers in A and X registers
+; Output: ResultX = X * A (16-bit result, high byte in ResultX, low byte in Temp)
+Multiply:
+    STA Temp
+    STX ResultX
+    LDY #0
+
+MultiplyLoop:
+    LSR Temp
+    BCC NoAdd
+    CLC
+    ADC ResultX
+NoAdd:
+    ROR ResultX
+    DEY
+    BNE MultiplyLoop
+
+    RTS
+
+; Define temporary storage for calculations
+CurrentAngle .res 1
+ResultX       .res 1
+ResultY       .res 1
+Temp          .res 1
